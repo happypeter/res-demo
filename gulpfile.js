5d4c9d10bcb99925c8c72572617ef9b33d5f587b
Peter Wang <happypeter1983@gmail.com>
2015-07-31 11:20:25 +0800

    add jeykky build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
+var cp = require('child_process');
+
+gulp.task('jekyll-build', function (done) {
+  return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+            .on('close', done);
+});
 
 gulp.task('sass', function () {
   return gulp.src('_scss/main.scss')
