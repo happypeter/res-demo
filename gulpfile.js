ace8dd1ac5fc4afd348a83bcb76fb23b7fe3d0e0
Peter Wang <happypeter1983@gmail.com>
2015-07-31 10:48:02 +0800

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
   return gulp.src('_scss/main.scss')
          .pipe(sass())
+         .pipe(prefix())
          .pipe(gulp.dest('./css'));
 });
 
